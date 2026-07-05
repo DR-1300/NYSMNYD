abbd608a537802c6934129d85d97e6d33fbb1c86
DR-1300 <dhruvi.rao90@gmail.com>
2026-07-05 17:59:43 +0530

    Created Caesar Cipher

---
+++ ciphers/caesar.py
@@ -0,0 +1,27 @@
+def encrypt(text, shift):
+    text1 = ""
+    for i in text:
+        ch = i
+        if i.isalpha():
+            if i.isupper():
+                ch = chr((ord(i) - ord('A') + shift) % 26 + ord('A'))
+            elif i.islower():
+                ch = chr((ord(i) - ord('a') + shift) % 26 + ord('a'))
+        text1 += ch
+    return text1
+
+def decrypt(text, shift):
+    text1 = ""
+    for i in text:
+        ch = i
+        if i.isalpha():
+            if i.isupper():
+                ch = chr((ord(i) - ord('A') - shift) % 26 + ord('A'))
+            elif i.islower():
+                ch = chr((ord(i) - ord('a') - shift) % 26 + ord('a'))
+        text1 += ch
+    return text1
+
+original = input("enter something: ")
+shift = int(input("shift: "))
+print("decrypted: " , decrypt(original, shift))
